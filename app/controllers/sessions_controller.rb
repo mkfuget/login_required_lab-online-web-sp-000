33c5a041091290b5cdf888f5e49c11df99772d17
Michael Fuget <michaelfuget@gmail.com>
2021-01-31 20:32:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,12 @@ class SessionsController < ApplicationController
   before_action :require_login
 
   def show
-    @document = Document.find(params[:id])
   end
 
   def index
   end
 
   def create
-    @document = Document.create(author_id: user_id)
   end
 
   private
